d489f74fc62d85462638c8e32655c8c366741788
azaz13 <elsa.martel@free.fr>
2016-04-25 15:19:19 +0200

    Modification menu ajout activité

--- app/src/main/java/com/example/niels/android/AjoutActivite.java
+++ app/src/main/java/com/example/niels/android/AjoutActivite.java
@@ -1,5 +1,6 @@
 package com.example.niels.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,19 +81,20 @@ public class AjoutActivite extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.autresActivite) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.accueil) {
+            Intent intent = new Intent(AjoutActivite.this, Accueil_Utilisateur.class);
+            startActivity(intent);
+        } else if (id == R.id.modifProfil) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } /*else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
-        }
+        }*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
