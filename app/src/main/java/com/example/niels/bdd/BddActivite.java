f9ebd9948874400b6b1e0a33af63eff3ceb20f0f
azaz13 <elsa.martel@free.fr>
2016-05-02 12:49:57 +0200

    Fonction BD

--- app/src/main/java/com/example/niels/bdd/BddActivite.java
+++ app/src/main/java/com/example/niels/bdd/BddActivite.java
@@ -26,6 +26,19 @@ public class BddActivite {
         bddProjet = new BddProjet(context);
     }
 
+    public void open(){
+        bdd = bddProjet.getWritableDatabase();
+    }
+
+    public void close(){
+        bdd.close();
+    }
+
+    public SQLiteDatabase getBdd(){
+        return bdd;
+    }
+
+
     public void addActivite(Activite activite)
     {
         ContentValues values = new ContentValues();
@@ -41,7 +54,7 @@ public class BddActivite {
     public Activite getActivitebyOwner(int idUser){
         bdd = bddProjet.getReadableDatabase();
 
-        String whereClause = COLONNE_ID_ACTIVITE + " = ? ";
+        String whereClause = COLONNE_ID_USER_ACTIVITE + " = ? ";
         String[] whereArgs = new String[] {
                 idUser+""
         };
@@ -72,16 +85,41 @@ public class BddActivite {
         return activite;
     }
 
-    public void open(){
-        bdd = bddProjet.getWritableDatabase();
+    public Activite getActivitebyIdActivite(int idActivite){
+        bdd = bddProjet.getReadableDatabase();
+
+        String whereClause = COLONNE_ID_ACTIVITE + " = ? ";
+        String[] whereArgs = new String[] {
+                idActivite+""
+        };
+        Cursor cursor = bdd.query(TABLE_ACTIVITE,
+                new String[]{COLONNE_ID_ACTIVITE, COLONNE_NOM_ACTIVITE, COLONNE_DESC_ACTIVITE,
+                        COLONNE_ID_USER_ACTIVITE, COLONNE_DATE_CREATION_ACTIVITE, COLONNE_TYPE},
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null,
+                null);
+
+        if(cursor != null){
+            cursor.moveToFirst();
+        }
+
+        if(cursor.getCount() <= 0)
+            return null;
+
+        Activite activite = new Activite();
+        activite.set_idActivite(Integer.parseInt(cursor.getString(0)));
+        activite.set_nomActivite(cursor.getString(1));
+        activite.set_description(cursor.getString(2));
+        activite.set_idUtilisateur(Integer.parseInt(cursor.getString(3)));
+        activite.set_dateCreation(cursor.getString(4));
+        activite.set_type(Integer.parseInt(cursor.getString(5)));
+        return activite;
     }
 
-    public void close(){
-        bdd.close();
-    }
 
-    public SQLiteDatabase getBdd(){
-        return bdd;
-    }
+
 
 }
--- app/src/main/java/com/example/niels/bdd/BddMembreActivite.java
+++ app/src/main/java/com/example/niels/bdd/BddMembreActivite.java
@@ -1,7 +1,12 @@
 package com.example.niels.bdd;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Elsa on 10/04/2016.
@@ -11,6 +16,8 @@ public class BddMembreActivite {
     private static final String TABLE_ACTIVITE_MEMBRE = "membreActivite";
     private static final String COLONNE_ID_USER_MEMBRE = "idUser";
     private static final String COLONNE_ID_ACTIVITE_MEMBRE = "idActivite";
+    private static final String COLONNE_DATE_INSCRIPTION_MEMBRE = "dateInscriptionMembre";
+
 
     private SQLiteDatabase bdd;
 
@@ -32,5 +39,66 @@ public class BddMembreActivite {
         return bdd;
     }
 
+    public void addMembreActivite(MembreActivite m){
+        ContentValues values = new ContentValues();
+        values.put(COLONNE_ID_ACTIVITE_MEMBRE, m.get_idActivite());
+        values.put(COLONNE_ID_USER_MEMBRE, m.get_idUtilisateur());
+        values.put(COLONNE_DATE_INSCRIPTION_MEMBRE, m.get_dateInscription());
+        bdd.insert(TABLE_ACTIVITE_MEMBRE, null, values);
+    }
+
+    public List<MembreActivite> getMembreByIdUser(int idUser){
+        List<MembreActivite> membreList = new ArrayList<MembreActivite>();
+        bdd = bddProjet.getReadableDatabase();
+
+        String whereClause = COLONNE_ID_USER_MEMBRE + " = ? ";
+        String[] whereArgs = new String[] {
+                idUser + ""
+        };
+        Cursor cursor = bdd.query(TABLE_ACTIVITE_MEMBRE,
+                new String[]{COLONNE_ID_USER_MEMBRE, COLONNE_ID_ACTIVITE_MEMBRE, COLONNE_DATE_INSCRIPTION_MEMBRE},
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null,
+                null);
+
+        if(cursor != null){
+            cursor.moveToFirst();
+        }
+
+        if(cursor.getCount() <= 0)
+            return null;
+
+        if (cursor.moveToFirst()){
+            do{
+                MembreActivite ma = new MembreActivite();
+                ma.set_idUtilisateur(Integer.parseInt(cursor.getString(0)));
+                ma.set_idActivite(Integer.parseInt(cursor.getString(1)));
+                ma.set_dateInscription(cursor.getString(2));
+                membreList.add(ma);
+            }while(cursor.moveToNext());
+        }
+        cursor.close();
+        return membreList;
+    }
+
+    public List<MembreActivite> getAllMembreActivite(){
+        List<MembreActivite> membreList = new ArrayList<MembreActivite>();
+        String query = "select * from " + TABLE_ACTIVITE_MEMBRE;
+        Cursor cursor = bdd.rawQuery(query, null);
+        if (cursor.moveToFirst()){
+            do{
+                MembreActivite ma = new MembreActivite();
+                ma.set_idUtilisateur(Integer.parseInt(cursor.getString(0)));
+                ma.set_idActivite(Integer.parseInt(cursor.getString(1)));
+                ma.set_dateInscription(cursor.getString(2));
+                membreList.add(ma);
+            }while(cursor.moveToNext());
+        }
+        cursor.close();
+        return membreList;
+    }
 
 }
