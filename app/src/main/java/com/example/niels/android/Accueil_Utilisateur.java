ff1ed7a9d7451948cd68a092e20471ae9b205a6f
azaz13 <elsa.martel@free.fr>
2016-05-09 14:22:17 +0200

    Page d'ajout d'evenement suite

--- app/src/main/java/com/example/niels/android/Accueil_Utilisateur.java
+++ app/src/main/java/com/example/niels/android/Accueil_Utilisateur.java
@@ -1,33 +1,21 @@
 package com.example.niels.android;
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.niels.AjoutEvenement;
 import com.example.niels.bdd.BddUser;
 import com.example.niels.bdd.User;
 
