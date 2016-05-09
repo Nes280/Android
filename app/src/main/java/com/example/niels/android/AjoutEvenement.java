a77132780d9958350e0bcfe2d5a64fcf245c964d
azaz13 <elsa.martel@free.fr>
2016-05-09 18:17:14 +0200

    Formulaire evenement presque fini

--- app/src/main/java/com/example/niels/android/AjoutEvenement.java
+++ app/src/main/java/com/example/niels/android/AjoutEvenement.java
@@ -1,8 +1,22 @@
 package com.example.niels.android;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.sax.TextElementListener;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,11 +26,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.niels.Code.getExemple;
 import com.example.niels.android.R;
+import com.example.niels.bdd.BddUser;
+import com.example.niels.bdd.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class AjoutEvenement extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, LocationListener  {
+
+    Intent intent =  null;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+    static final int REQUEST_LOCATION = 2;
+    String rep = null;
+    private LocationManager locationManager;
+    Location location;
+    LocationManager lm;
+    String bestProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +77,152 @@ public class AjoutEvenement extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Recuperation des informations de l'évenement
+        final TextView nomEvenement = (TextView) findViewById(R.id.nom);
+        final TextView descriptionEvenement = (TextView) findViewById(R.id.description);
+
+        ((Button) findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Recuperation des textes
+                String nomE = nomEvenement.getText().toString();
+                String descriptionE = descriptionEvenement.getText().toString();
+
+                //Verifier que l'utilisateur a rempli tous les champs
+                if(nomE.isEmpty() || descriptionE.isEmpty()){
+                    Toast.makeText(AjoutEvenement.this, R.string.verifChamps, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                //Verification que l'evenement est unique par rapport à l'activité
+                //A faire mais pas script
+
+                //Recuperer la date du jour
+                String format = "dd/MM/yy H:mm:ss";
+                java.text.SimpleDateFormat formater = new java.text.SimpleDateFormat(format);
+                java.util.Date dateJava = new java.util.Date();
+                System.out.println(formater.format(dateJava));
+                String date = formater.format(dateJava);
+
+                //récuperation du pseudo
+                BddUser db = new BddUser(AjoutEvenement.this);
+                db.open();
+                User u = db.getUserByIsConnected();
+                String pseudo = u.get_pseudo();
+                db.close();
+
+                //Demande de permission
+                int permissionCheck = ContextCompat.checkSelfPermission(AjoutEvenement.this,
+                        Manifest.permission.INTERNET);
+
+                int permissionCheck2 = ContextCompat.checkSelfPermission(AjoutEvenement.this,
+                        Manifest.permission.ACCESS_NETWORK_STATE);
+
+                int permissionCheck3 = ContextCompat.checkSelfPermission(AjoutEvenement.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION);
+
+                if (permissionCheck == PackageManager.PERMISSION_GRANTED && permissionCheck2 == PackageManager.PERMISSION_GRANTED
+                        && permissionCheck3 == PackageManager.PERMISSION_GRANTED) {
+                    //Toast.makeText(Inscription.this, "Permission", Toast.LENGTH_LONG).show();
+                    ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                    if (networkInfo != null && networkInfo.isConnected()) {
+
+                        //Recuperer l'id de l'utilisateur
+                        AccesBD a = new AccesBD();
+                        String urlUtilisateur = "/Android/recupUtilisateur.php?pseudo="+pseudo;
+                        a.execute(urlUtilisateur);
+                        try {
+                            a.get();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        }
+
+                        String response = rep;
+                        String valeur = null;
+                        String identifiantUtilisateur = null;
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            //JSONObject newJson = jsonObject.getJSONObject("state");
+                            valeur = jsonObject.getString("state");
+                            Log.e("resultat json " , valeur);
+                            if(valeur.equals("0")){
+                                Toast.makeText(AjoutEvenement.this, R.string.verifExistancePseudo, Toast.LENGTH_LONG).show();
+                                return;
+                            }
+                            JSONObject utilisateur = jsonObject.getJSONObject("utilisateur");
+                            identifiantUtilisateur = utilisateur.getString("id");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        //Recuperer l'id de l'activité
+                        //A faire
+
+                        //Recuperer la latitude et la longitude
+                        lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+                        boolean gps_enable = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+                        boolean network_enable = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+                        Log.e("gps ", gps_enable + "");
+                        Log.e("network", network_enable + "");
+
+                        location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                        if(location != null){
+                            double longitude = location.getLongitude();
+                            double latitude = location.getLatitude();
+
+                            Log.e("latitude", latitude + "");
+                            Log.e("longitude", longitude + "");
+                        }
+                        else
+                        {
+                            //A faire
+                            Log.e("location ", "null");
+                            Toast.makeText(AjoutEvenement.this, "Activez la localisation", Toast.LENGTH_LONG).show();
+                            return;
+                        }
+
+                        /*intent = new Intent(AjoutEvenement.this, Accueil_Utilisateur.class);
+                        startActivity(intent);*/
+                    } else {
+                        Toast.makeText(AjoutEvenement.this, R.string.demandeDeConnexion, Toast.LENGTH_LONG).show();
+                    }
+                } else
+                {
+                    ActivityCompat.requestPermissions(AjoutEvenement.this,
+                            new String[]{Manifest.permission.INTERNET},
+                            REQUEST_CODE_ASK_PERMISSIONS);
+
+                    ActivityCompat.requestPermissions(AjoutEvenement.this,
+                            new String[]{Manifest.permission.ACCESS_NETWORK_STATE},
+                            REQUEST_CODE_ASK_PERMISSIONS);
+
+                    ActivityCompat.requestPermissions(
+                            AjoutEvenement.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            1);
+
+                    boolean i = checkLocationPermission();
+
+                    Log.e("erreur", "permission denied " + i);
+
+                }
+
+            }
+        });
+    }
+
+    public boolean checkLocationPermission()
+    {
+        String permission = "android.permission.ACCESS_FINE_LOCATION";
+        int res = this.checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
     }
 
     @Override
@@ -99,5 +280,52 @@ public class AjoutEvenement extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    private class AccesBD extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                getExemple e = new getExemple();
+                String host = "http://folionielsbenichou.franceserv.com";
+                //String rep = null;
+
+                //rep = e.run(host+"/Android/nouvelUtilisateur.php?nom=" +n+"&prenom="+p+"&pseudo="+ps+"&motDePasse="+mdpHash+"&date="+date);
+                rep = e.run(host+params[0]);
+                Log.e("REPOSE", rep);
+                //return downloadUrl(params[0]);
+                return rep;
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL maybe invalide ";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            rep = result;
+            //Log.e("rep" , " res " + result);
+            //Toast.makeText(Inscription.this, "Response " + result, Toast.LENGTH_LONG).show();
+        }
     }
 }
