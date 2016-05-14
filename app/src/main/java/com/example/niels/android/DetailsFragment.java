a47c7cf7ded64c6614531c8fbcadb66317d2c214
Nes280 <Nes280@users.noreply.github.com>
2016-05-14 10:57:06 +0200

    contenue du fragment pour activité ok, il ne rest plus qu'à faire l'affichage.

--- app/src/main/java/com/example/niels/android/DetailsFragment.java
+++ app/src/main/java/com/example/niels/android/DetailsFragment.java
@@ -6,8 +6,13 @@ import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
+import com.example.niels.bdd.Activite;
+
+import java.util.ArrayList;
 
 /**
  * Created by Elsa on 04/04/2016.
@@ -18,39 +23,46 @@ public class DetailsFragment extends Fragment{
      * Create a new instance of DetailsFragment, initialized to
      * show the text at 'index'.
      */
-    public static DetailsFragment newInstance(int index) {
+    public static DetailsFragment newInstance( String[] list, int index) {
         DetailsFragment f = new DetailsFragment();
 
         // Supply index input as an argument.
         Bundle args = new Bundle();
+        args.putStringArray("list", list );
         args.putInt("index", index);
         f.setArguments(args);
 
         return f;
     }
 
-    public int getShownIndex() {
-        return getArguments().getInt("index", 0);
+    public String[] getShownList() {
+        return getArguments().getStringArray("list");
+    }
+
+    public int getShownId() {
+        return getArguments().getInt("index");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //TODO recuperer l'acivité en fonction de son id
         if (container == null) {
 
             return null;
         }
-
+        String[] list = getShownList();
+        View detailsFrame = getActivity().findViewById(R.id.fragment);
         ScrollView scroller = new ScrollView(getActivity());
-        TextView text = new TextView(getActivity());
+        TextView nomActivite = new TextView(getActivity());
         int padding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
                 4, getActivity().getResources().getDisplayMetrics());
-        text.setPadding(padding, padding, padding, padding);
-        scroller.addView(text);
-        text.setText("Bla Bla");//Shakespeare.DIALOGUE[getShownIndex()]);
+        nomActivite.setPadding(padding, padding, padding, padding);
+        scroller.addView(nomActivite);
+        nomActivite.setText(list[1] + " "+ list[2]);
         return scroller;
 
-        //return inflater.inflate(R.layout.content2_accueil,container, false);
+
 
     }
 }
