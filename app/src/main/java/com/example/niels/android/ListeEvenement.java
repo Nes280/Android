a306bae93abe0c30b8f33682b49b510528014ba1
Nes280 <Nes280@users.noreply.github.com>
2016-05-16 21:34:51 +0200

    evenements suite

--- app/src/main/java/com/example/niels/android/ListeEvenement.java
+++ app/src/main/java/com/example/niels/android/ListeEvenement.java
@@ -1,20 +1,31 @@
 package com.example.niels.android;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.niels.Code.getExemple;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 
 public class ListeEvenement extends AppCompatActivity {
     private ListView mListView;
     private ArrayAdapter<String> adapter;
+    String rep = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +48,88 @@ public class ListeEvenement extends AppCompatActivity {
         Bundle b = getIntent().getExtras();
         String id = (String)b.get("id");
         ArrayList<String> evenements = new ArrayList<String>();
+
+        //on recupère les evenements en BD
+        String url = "/Android/recupEvenement.php?activite=" + id;
+        AccesBD recActivite = new AccesBD();
+        recActivite.execute(url);
+        try {
+            recActivite.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        String response2 = rep;
+        String valeur2 = null;
+        int idEve = 0;
+        double lat = 0.0;
+        double lon = 0.0;
+        String nom = "";
+        String desc = "";
+        String photo = "";
+        String date = "";
+        int idUser = 0;
+
+        try {
+            JSONObject jsonEvenements = new JSONObject(rep);
+            JSONArray jsonEveInfo = jsonEvenements.getJSONArray("evenement");
+
+            for (int i = 0; i<jsonEveInfo.length();i++)
+            {
+                JSONObject objNomActivite = jsonEveInfo.getJSONObject(i);
+                idEve = Integer.parseInt(objNomActivite.getString("id evenement") + "");
+                idUser = Integer.parseInt(objNomActivite.getString("idUtilisateur") + "");
+                lat = Double.parseDouble(objNomActivite.getString("latitude GPS") + "");
+                lon = Double.parseDouble(objNomActivite.getString("longitude GPS") + "");
+                nom = objNomActivite.getString("nom evenement");
+                desc = objNomActivite.getString("description");
+                photo = objNomActivite.getString("photo");
+                date = objNomActivite.getString("date");
+
+                evenements.add(nom);
+                evenements.add(getString(R.string.description)+" : "+desc);
+                evenements.add(getString(R.string.date)+" : "+date);
+                evenements.add(getString(R.string.photo));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         evenements.add(id);
         adapter = new ArrayAdapter<String>(this, R.layout.simple_row, evenements);
         mListView.setAdapter(adapter);
 
 
     }
+    private class AccesBD extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                getExemple e = new getExemple();
+                String host = "http://folionielsbenichou.franceserv.com";
+                //String rep = null;
+
+                //rep = e.run(host+"/Android/nouvelUtilisateur.php?nom=" +n+"&prenom="+p+"&pseudo="+ps+"&motDePasse="+mdpHash+"&date="+date);
+                rep = e.run(host+params[0]);
+                Log.e("REPOSE", rep);
+                //return downloadUrl(params[0]);
+                return rep;
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL maybe invalide ";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            rep = result;
+            //Log.e("rep" , " res " + result);
+            //Toast.makeText(Inscription.this, "Response " + result, Toast.LENGTH_LONG).show();
+        }
+    }
+
 
 }
