7a1982c457a21a1fb32a627ab776fc3fc050a114
Nes280 <Nes280@users.noreply.github.com>
2016-05-17 22:25:36 +0200

    google map et details de l'evenement sur une seule page

--- app/src/main/java/com/example/niels/android/ListeEvenement.java
+++ app/src/main/java/com/example/niels/android/ListeEvenement.java
@@ -1,6 +1,8 @@
 package com.example.niels.android;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,11 +11,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.example.niels.Code.getExemple;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,16 +30,30 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.concurrent.ExecutionException;
 
 
 public class ListeEvenement extends AppCompatActivity {
     private ListView mListView;
     private ArrayAdapter<String> adapter;
+    ArrayList<String> paramEve = new ArrayList<String>();
     String rep = null;
     Intent intent = null;
-    double lat = 0.0;
-    double lon = 0.0;
+    String lat = "";
+    String lon = "";
+    String idEve = "";
+    String nom = "";
+    String desc = "";
+    String photo = "";
+    String date = "";
+    String idUser = "";
+    Hashtable<Integer, ArrayList<String>> contenueEvenement = new Hashtable<Integer, ArrayList<String>>();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +72,11 @@ public class ListeEvenement extends AppCompatActivity {
         });
 
 
-
         mListView = (ListView) findViewById(R.id.listView);
+        ScrollView liste = (ScrollView) findViewById(R.id.scrollView);
 
         Bundle b = getIntent().getExtras();
-        String id = (String)b.get("id");
+        String id = (String) b.get("id");
         ArrayList<String> evenements = new ArrayList<String>();
 
         //on recupère les evenements en BD
@@ -70,40 +93,44 @@ public class ListeEvenement extends AppCompatActivity {
 
         String response = rep;
         String valeur = null;
-        int idEve = 0;
 
-        String nom = "";
-        String desc = "";
-        String photo = "";
-        String date = "";
-        int idUser = 0;
-//TODO tester si il y a des evenements
+
         try {
             JSONObject jsonEvenements = new JSONObject(rep);
             valeur = jsonEvenements.getString("state");
-            if (valeur.equals("1"))
-            {
+            if (valeur.equals("1")) {
                 JSONArray jsonEveInfo = jsonEvenements.getJSONArray("evenement");
 
-                for (int i = 0; i<jsonEveInfo.length();i++)
-                {
+                for (int i = 0; i < jsonEveInfo.length(); i++) {
+                    //paramEve.clear();
                     JSONObject objNomActivite = jsonEveInfo.getJSONObject(i);
-                    idEve = Integer.parseInt(objNomActivite.getString("id evenement") + "");
-                    idUser = Integer.parseInt(objNomActivite.getString("idUtilisateur") + "");
-                    lat = Double.parseDouble(objNomActivite.getString("latitude GPS") + "");
-                    lon = Double.parseDouble(objNomActivite.getString("longitude GPS") + "");
+                    idEve = objNomActivite.getString("id evenement");
+                    idUser = objNomActivite.getString("idUtilisateur");
+                    lat = objNomActivite.getString("latitude GPS");
+                    lon = objNomActivite.getString("longitude GPS");
                     nom = objNomActivite.getString("nom evenement");
                     desc = objNomActivite.getString("description");
                     photo = objNomActivite.getString("photo");
                     date = objNomActivite.getString("date");
 
-                    evenements.add(nom);
-                    evenements.add(getString(R.string.description)+" : "+desc);
-                    evenements.add(getString(R.string.date)+" : "+date);
-                    evenements.add(getString(R.string.photo));
+                    evenements.add("\n" + nom.toUpperCase() + "\n\n" +
+                                    getString(R.string.description) + " : " + desc + "\n" +
+                                    getString(R.string.date) + " : " + date + "\n" +
+                                    getString(R.string.photo) + "\n"
+                    );
+
+                    paramEve.add(idEve);
+                    paramEve.add(idUser);
+                    paramEve.add(lat);
+                    paramEve.add(lon);
+                    paramEve.add(nom);
+                    paramEve.add(desc);
+                    paramEve.add(photo);
+                    paramEve.add(date);
+
+                    contenueEvenement.put(i,paramEve);
                 }
-            }
-            else  evenements.add(getString(R.string.no_event));
+            } else evenements.add(getString(R.string.no_event));
 
 
         } catch (JSONException e) {
@@ -111,18 +138,85 @@ public class ListeEvenement extends AppCompatActivity {
         }
 
         adapter = new ArrayAdapter<String>(this, R.layout.simple_row, evenements);
-        mListView.setAdapter(adapter);
-        ((Button) findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                intent = new Intent(ListeEvenement.this, DetailsEvent.class);
-                intent.putExtra("lat", lat);
-                intent.putExtra("lon", lon);
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Log.e("LOG---->", contenueEvenement.get(position).toString());
+                ArrayList<String>liste = contenueEvenement.get(position);
+                Intent intent = new Intent();
+                //Log.e("LOG####",liste.get((position ) * 8) + 4+" "+liste.get((position ) * 8) + 2+)
+                intent.setClass(ListeEvenement.this, MapEvenement.class);
+                intent.putExtra("nom", liste.get((((position) * 8) + 4)));
+                intent.putExtra("desc", liste.get((((position ) * 8) + 5)));
+                intent.putExtra("date", liste.get((((position ) * 8) + 7)));
+                intent.putExtra("lat", liste.get((((position ) * 8) + 2)));
+                intent.putExtra("lon", liste.get((((position ) * 8) + 3)));
                 startActivity(intent);
             }
         });
 
+        mListView.setAdapter(adapter);
+
+
+        /*((Button) findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(ListeEvenement.this, MapEvenement.class);
+                intent.putExtra("lat", lat);
+                intent.putExtra("lon", lon);
+                startActivity(intent);
+            }
+        });*/
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "ListeEvenement Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.niels.android/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "ListeEvenement Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.niels.android/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
     private class AccesBD extends AsyncTask<String, Void, String> {
 
         @Override
@@ -133,7 +227,7 @@ public class ListeEvenement extends AppCompatActivity {
                 //String rep = null;
 
                 //rep = e.run(host+"/Android/nouvelUtilisateur.php?nom=" +n+"&prenom="+p+"&pseudo="+ps+"&motDePasse="+mdpHash+"&date="+date);
-                rep = e.run(host+params[0]);
+                rep = e.run(host + params[0]);
                 Log.e("REPOSE", rep);
                 //return downloadUrl(params[0]);
                 return rep;
--- app/src/main/java/com/example/niels/android/MapEvenement.java
+++ app/src/main/java/com/example/niels/android/MapEvenement.java
@@ -2,6 +2,8 @@ package com.example.niels.android;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,20 +12,37 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DetailsEvent extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class MapEvenement extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     double lat = 0.0;
     double lon = 0.0;
+    ArrayList<String> paramEve = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details_event);
+        setContentView(R.layout.activity_details_evenement);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Bundle b = getIntent().getExtras();
+       // paramEve = (ArrayList<String>)b.get("array");
+
+        TextView nom = (TextView) findViewById(R.id.textView);
+        TextView desc = (TextView) findViewById(R.id.textView2);
+        TextView date = (TextView) findViewById(R.id.textView3);
+
+        nom.setText((String) b.get("nom"));
+        String de = (String) b.get("desc");
+        String da = (String) b.get("date");
+        desc.setText(getString(R.string.description) + " : "+de);
+        date.setText(getString(R.string.date) + " : "+da);
+
     }
 
 
@@ -41,12 +60,14 @@ public class DetailsEvent extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         Bundle b = getIntent().getExtras();
-        lat = (Double)b.get("lat");
-        lon = (Double)b.get("lon");
+        paramEve = (ArrayList<String>)b.get("array");
+        lat = Double.parseDouble((String)b.get("lat"));
+        lon = Double.parseDouble((String)b.get("lon"));
+        Log.e("LAT LON : ",lat+" "+lon);
 
         // Add a marker in Sydney and move the camera
         LatLng ici = new LatLng(lat, lon);
-        mMap.addMarker(new MarkerOptions().position(ici).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(ici).title(getString(R.string.location_event)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(ici));
     }
 }
